c96fca0df0098a85635d4b5826cc468bf030be71
Daniel Nilsson <daniel.nilsson@apegroup.com>
2018-02-23 14:17:19 +0100

    Upgrades to Swift 4

--- Source/APESuperHUD.swift
+++ Source/APESuperHUD.swift
@@ -247,7 +247,7 @@ public class APESuperHUD {
     */
     public static func removeHUD(animated: Bool, presentingView: UIView, completion: (() -> Void)?) {
         if let hudView = getHudView(presentingView: presentingView) {
-            hudView.removeHud(animated: animated, onDone: { _ in
+            hudView.removeHud(animated: animated, onDone: {
                 completion?()
             })
         }
@@ -265,7 +265,7 @@ public class APESuperHUD {
         if hudView.isActivityIndicatorSpinnning {
 
             // Hide HUD view, and call same function when it's done
-            hudView.hideLoadingActivityIndicator(completion: { _ in
+            hudView.hideLoadingActivityIndicator(completion: {
                 
                 showHud(text: text, icon: icon, duration: duration, sksFileName: sksFileName, presentingView: presentingView, completion: completion)
                 return
